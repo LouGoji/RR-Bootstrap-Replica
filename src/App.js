6a8a12dae50dcd2fdeb7f9928206b89c1d7e8451
Louis Mantell <loiuismantell@Louis-iMac.local>
2022-11-29 18:20:15 -0500

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import './App.css';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="light" expand="lg">
+          <Navbar.Brand style={{margin:"5px 0 0 20px"}}>
+            <img
+              src="./Instacart_Logo.jpg"
+              alt="Instacart logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+          <Navbar.Collapse>
+            <Nav className="mr-auto">
+            </Nav>
+            <Nav>
+              <Nav.Link href="#Login">Log In</Nav.Link>
+              <Button variant="success">Sign Up</Button>{' '}
+            </Nav>
+          </Navbar.Collapse>
+      </Navbar>
+
       <h1>Hello World</h1>
     </div>
   );
